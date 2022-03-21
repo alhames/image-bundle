e062cb82238a9e13c6dce6afad4d92780938224c
Alhames <alhames@mail.ru>
2022-03-21 23:33:11 +0300

    init

---
+++ src/DependencyInjection/AlhamesImageExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alhames\ImageBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class AlhamesImageExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Alhames\ImageBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('alhames_image');
+
+        return $treeBuilder;
+    }
+}
